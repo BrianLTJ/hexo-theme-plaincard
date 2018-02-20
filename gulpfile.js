2d5fffc67bf2fbb3affec7cc870027a9832fb328
Tingjun Li <739142578@qq.com>
2018-02-20 21:22:40 +0800

    Gulp useref imports in ejs templates

--- gulpfile.js
+++ gulpfile.js
@@ -8,85 +8,106 @@ var del = require('del');
 var rev = require('gulp-rev');
 var useref = require('gulp-useref');
 var gulpif = require('gulp-if');
+var filter = require('gulp-filter');
+var revReplace = require('gulp-rev-replace');
 
-gulp.task('clean-css', function(){
+gulp.task('clean-build', function(){
     return del([
-        'build/css/*'
-    ])
-});
-gulp.task('clean-js', function(){
-    return del([
-        'build/js/*'
-    ])
-});
-gulp.task('clean-html', function(){
-    return del([
-        'build/*.html',
-        'build/*.htm',
-        'build/images/*'
-    ])
-});
-gulp.task('clean-rev-manifest', function(){
-    return del([
-        'build/rev-manifest.json'
-    ])
-});
-gulp.task('clean-images', function(){
-    return del([
-        'build/images/*'
+        'build/css/*',
+        'build/js/*',
+        'build/images/*',
+        'build/vendor/*'
     ])
 });
 
-gulp.task('html', ['clean-html', 'clean-rev-manifest'],function(){
-    return gulp.src('./*.html')
+
+
+gulp.task('html-build', function(){
+    var jsFilter = filter("**/*.js", { restore: true });
+    var cssFilter = filter("**/*.css", { restore: true });
+    var indexHtmlFilter = filter(['**/*', '!**/index.html'], { restore: true });
+    return gulp.src('index.html')
         .pipe(useref())
-        .pipe(gulpif('*.css', minifyCss()))
-        .pipe(gulp.dest('build'))
-});
-
-gulp.task('images', ['clean-images'], function(){
-    return gulp.src(['./images/*.jpg', './images/*.jpeg', './images/*.png', './images/*.bmp', './images/*.svg'])
-        .pipe(gulp.dest('build/images'))
-});
-
-gulp.task('css', ['clean-css'],function(){
-    return gulp.src('css/*.css')
+        .pipe(jsFilter)
+        .pipe(minify())
+        .pipe(jsFilter.restore)
+        .pipe(cssFilter)
         .pipe(minifyCSS())
-        .pipe(rev('revcss'))
-        .pipe(gulp.dest('build/css'))
-        .pipe(rev.manifest({merge:true}))
-        .pipe(gulp.dest('build/'))
+        .pipe(cssFilter.restore)
+        .pipe(indexHtmlFilter)
+        .pipe(rev())
+        .pipe(indexHtmlFilter.restore)
+        .pipe(revReplace())
+        .pipe(gulp.dest('build'));
 });
 
-gulp.task('js', ['clean-js'],function(){
-    return gulp.src('js/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(rev('revjs'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('build/js'))
-        .pipe(rev.manifest({
-            merge:true
-        }))
-        .pipe(gulp.dest('build/'))
+gulp.task('default', ['html-build', 'images', 'css', 'js'])
+
+
+/*
+ * Build EJS distribution bundles
+ * 
+ */
+gulp.task('ejs-clean:config', function(){ return del(['dist/*.yml']); });
+
+gulp.task('ejs-clean:layout', function(){ return del(['dist/layout/*']); });
+
+gulp.task('ejs-clean:languages', function(){ return del(['dist/layout/*']); });
+
+gulp.task('ejs-clean:scripts', function(){ return del(['dist/layout/*']); });
+
+gulp.task('ejs-clean:source', function(){ return del(['dist/layout/*']); });
+
+gulp.task('build-dist:source-links', ['ejs-clean:layout', 'ejs-clean:source'], function(){
+    var jsFilter = filter("**/*.js", { restore: true });
+    var cssFilter = filter("**/*.css", { restore: true });
+    var ejsFilter = filter(['**/*', '!**/index.ejs', '!**/*.ejs'], { restore: true });
+    var nojscssFilter = filter(['**/*', '!**/*.js', '!**/*.css'], { restore: true });
+    var onlyejsFilter = filter("**/*.ejs", { restore: true });
+    return gulp.src('layout/import/*.ejs')
+        .pipe(useref({ 
+            transformPath: function(filePath) {
+                return filePath.replace('./','../../')
+                } 
+            }))
+        .pipe(jsFilter)
+        .pipe(minify())
+        .pipe(jsFilter.restore)
+        .pipe(cssFilter)
+        .pipe(minifyCSS())
+        .pipe(cssFilter.restore)
+        .pipe(ejsFilter)
+        .pipe(rev())
+        .pipe(gulp.dest('dist/source/')) // Write css js vendors to dist/source/
+        .pipe(ejsFilter.restore)
+        .pipe(revReplace({replaceInExtensions:['.ejs']}))
+        .pipe(onlyejsFilter)
+        .pipe(gulp.dest('dist/layout/import')); // Write import ejs.
 });
 
-gulp.task('ejs', ['clean-build-ejs'], function(){
-    return gulp.src('index.ejs')
-        .pipe(ejs())
-        .pipe(gulp.dest('build-ejs'))
+gulp.task('build-dist:config', ['ejs-clean:config'],function(){ 
+    return gulp.src('*.yml')
+        .pipe(gulp.dest('dist/')); 
 });
 
-gulp.task('clean-build-ejs', function(){
-    return del([
-        'build-ejs/*'
-    ]);
+gulp.task('build-dist:layout', ['ejs-clean:layout'], function(){
+    return gulp.src('layout/*')
+        .pipe(gulp.dest('dist/layout'));
 });
 
-gulp.task('jquery', function(){
-    return gulp.src()
+gulp.task('build-dist:languages', ['ejs-clean:languages'], function(){ 
+    return gulp.src('languages/*')
+        .pipe(gulp.dest('dist/languages')); 
+    });
+
+gulp.task('build-dist:scripts', ['ejs-clean:scripts'], function(){ 
+    return gulp.src('scripts/*')
+        .pipe(gulp.dest('dist/scripts'));
+    });
+
+gulp.task('build-dist:source', ['ejs-clean:source'], function(){
+    return gulp.src('source/*')
+        .pipe(gulp.dest('dist/source'));
 });
 
-gulp.task('default', ['html', 'images', 'css', 'js'])
-
-
-gulp.task('build-js', ['ejs'])
+gulp.task('build-dist',['build-dist:config','build-dist:layout','build-dist:languages','build-dist:scripts','build-dist:scripts']);
